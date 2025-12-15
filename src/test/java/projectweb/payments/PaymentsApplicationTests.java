45519134fb1cadcd0927ff0ebae3ea1407f5205d
wonehgus <wonehgus1234@naver.com>
2025-12-15 16:55:46 +0900

    Initial commit

---
+++ src/test/java/projectweb/payments/PaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package projectweb.payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
